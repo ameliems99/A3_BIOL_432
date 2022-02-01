4f67b2e222aab652d6beb17d0a1f9f94c3c6a54f
Amelie <16agms@queensu.ca>
2022-02-01 12:42:28 -0500

    A4 PartII Update

--- A4_MAHRTSMITH_AMELIE_MyCount.sh
+++ A4_MAHRTSMITH_AMELIE_MyCount.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-
 for i in {1..20}
 do
- if [ ${#i} == 1 ]
- then
-  echo "$i is a single-digit number"
- elif [ ${#i} == 2 ]
- then
-  echo "$i is a double-digit number"
- fi
+  if [ ${#i} == 1 ]
+  then x="single"
+  elif [ ${#i} == 2 ]
+  then x="double"
+  fi
+  echo "$i is a $x-digit number"
 done
