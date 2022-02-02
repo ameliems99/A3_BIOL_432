888d79aef396e7155e5a3479071241aec952afe0
Amelie <16agms@queensu.ca>
2022-02-02 07:02:41 -0500

    A4 final update

--- A4_MAHRTSMITH_AMELIE_MyCount.sh
+++ A4_MAHRTSMITH_AMELIE_MyCount.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-for i in {1..20}
+for i in {1..20}  #loop through a sequence of 20 numbers
 do
-  if [ ${#i} == 1 ]
+  if [ ${#i} == 1 ]  #if number of digits in the iteration count is 1
   then x="single"
-  else x="double"
+  else x="double"  #if number of digits in the iteration count is 2
   fi
-  echo "$i is a $x-digit number"
+  echo "$i is a $x-digit number"  #print number of iterations and if single/double digit
 done
